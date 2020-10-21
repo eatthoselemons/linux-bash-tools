c2c1c14cc4c6df5c6d33648b72701287c975f4e0
ethan <preevete@gmail.com>
2020-10-21 15:55:49 -0600

    added example of using string for array of config values

--- libs/examples/config-file-manipulation/read-var-example.sh
+++ libs/examples/config-file-manipulation/read-var-example.sh
@@ -1,5 +1,16 @@
 source ../../config.shlib
 val=$(getConfigVar example.cfg myvar)
 val2=$(getConfigVar example.cfg item)
+myStringVal=$(getConfigVar example.cfg string)
+
+printf -- "%s\n" "echoing the values from the config file"
 echo $val
 echo $val2
+echo $myStringVal
+echo
+
+printf -- "%s\n" "looping through the values in the config file"
+for val in $myStringVal
+do
+  printf -- "%s\n" $val
+done
